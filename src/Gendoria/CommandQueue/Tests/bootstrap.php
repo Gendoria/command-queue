112d7fdf6e0094bcbebafa9b81250ed2ca1af485
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2016-09-30 16:16:01 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Gendoria/CommandQueue/Tests/bootstrap.php
+++ src/Gendoria/CommandQueue/Tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 call_user_func(function() {
-    $autoloadFile = __DIR__ . '/../../../../vendor/autoload.php';
+    $autoloadFile = __DIR__.'/../../../../vendor/autoload.php';
     if (!is_file($autoloadFile)) {
         throw new LogicException('Could not find vendor/autoload.php. Did you forget to run "composer install --dev"?');
     }
